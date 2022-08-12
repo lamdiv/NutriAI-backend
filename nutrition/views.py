b8a37874aaf1389e7c721d7545fa6b37e05d13e6
Ubuntu <ubuntu@ip-172-31-36-175.us-east-2.compute.internal>
2022-08-12 05:23:45 +0000

    :sparkles: opearating scan feature finsih along with filter retrive of nutrition commodity

--- nutrition/views.py
+++ nutrition/views.py
@@ -6,16 +6,40 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.http import HttpResponse
 import easyocr
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 
 class NutritionCommodityView(viewsets.ModelViewSet):
 	queryset = NutritionCommodity.objects.all()
 	serializer_class = NutritionCommoditySerializer
-
+	filter_backends = (SearchFilter, OrderingFilter)
+	search_fields = ('user',
+					'food_commodity',
+					'edible_portion',
+					'moisture_g',
+					'protein_g',
+					'fat_g',
+					'carbohydrate_h',
+					'mineral_g',
+					'fiber_g',
+					'energy_k_cal',
+					'calcium_mg',
+					'phosphorous_mg',
+					'iron_mg',
+					'carotene_miu_g',
+					'vitamin_c_mg',
+					'thiamine_mg',
+					'riboflavin_mg'
+					'niacin_mg')
+	# ordering_fields = ('host_to_id__post_date','host_to_id__last_edited')
+	ordering = ['-created_date', '-nutrition_id']
 
 @api_view(['POST',])
 def image_upload_and_scan(request):
-    return Response({'method': 'POST', 'data': request.data})
+	image_to_scan_url = request.data["image_to_scan"]
+	reader = easyocr.Reader(["en"])
+	results = reader.readtext(image_to_scan_url)	
+	return Response({'method': 'POST', 'image_to_scan_url': image_to_scan_url, "results": results})
 
 # http://localhost:8000/media/scan_pics/2022-08-12%2003%3A13%3A04.520690%2B00%3A00/mallasahajraj%40gmail.com/Screen_Shot_2022-07-1_IvCuiwC.png
 
@@ -28,14 +52,14 @@ class ScanningImageView(viewsets.ModelViewSet):
 
 
 	def post(self, request, *args, **kwargs):
-		user_id = request.data['user_id']
+		user_id = request.data['user']
 		image_to_scan = request.data['image_to_scan']
 		ScanningImage.objects.create(user_id=user_id, image_to_scan=image_to_scan)
 
-		ScanningImage.objects.all().order_by('-id')[0].image_to_scan.url
-		reader = easyocr.Reader(["en"])
+		# image_path = ScanningImage.objects.all().order_by('-image_id')[0].image_to_scan.url
+		# reader = easyocr.Reader(["en"])
+		# print(image_path)
+		# results = reader.readtext(image_path)	
 
-		results = reader.readtext(ScanningImage)
 
-
-		return HttpResponse({'message': results}, status=200)
+		# return Response({"results": results})
